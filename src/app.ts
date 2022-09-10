9b8c868417b5cb41e894220fa2560cff26d3b54c
Felipe Martinez <pipernene@gmail.com>
2022-09-10 14:16:05 +0200

    Document all the things

--- src/app.ts
+++ src/app.ts
@@ -17,6 +17,9 @@ interface FailedResponse {
     message: string;
 }
 
+/**
+ * Base class for all requests, handles sending request and parsing the response.
+ */
 abstract class ApiRequest<T> {
     protected readonly query = new URLSearchParams();
 
@@ -31,6 +34,7 @@ abstract class ApiRequest<T> {
         } catch (e) {
             if (axios.isAxiosError(e)) {
                 const resp = e.response.data as FailedResponse;
+
                 throw new Error(`Request failed: ${resp.message}`);
             } else {
                 throw e;
@@ -39,22 +43,41 @@ abstract class ApiRequest<T> {
     }
 }
 
+/**
+ * Base class for requests that return multiple items that can be paged, filtered and sorted.
+ */
 class ApiRequestMany<T> extends ApiRequest<T> {
+    /**
+     * Limits the number of items returned.
+     * @param n Maximum number of items.
+     */
     limit(n: number): Omit<this, "limit"> {
         this.query.set("limit", String(n));
         return this;
     }
 
+    /**
+     * Selects which page to return.
+     * @param n Page number, starting from 1.
+     */
     page(n: number): Omit<this, "page"> {
         this.query.set("page", String(n));
         return this;
     }
 
+    /**
+     * Offsets the returned items within a page.
+     */
     offset(n: number): Omit<this, "offset"> {
         this.query.set("offset", String(n));
         return this;
     }
 
+    /**
+     * Sorts the returned items by a field.
+     * @param field The field to sort by.
+     * @param direction The direction to sort in.
+     */
     sort(field: keyof ItemType<T>, direction: "asc" | "desc"): Omit<this, "sort"> {
         this.query.set("sort", `${String(field)}:${direction}`);
         return this;
@@ -97,9 +120,13 @@ class ApiRequestMany<T> extends ApiRequest<T> {
     }
 }
 
+/**
+ * Base class for requests that return a single item.
+ */
 class ApiRequestSingle<T> extends ApiRequest<T> {
     async get(): Promise<T> {
         const { docs } = await this.fetch();
+
         if (docs.length == 0) {
             throw new Error("No items in response");
         }
@@ -108,6 +135,10 @@ class ApiRequestSingle<T> extends ApiRequest<T> {
     }
 }
 
+/**
+ * Creates an API client that can execute authenticated queries.
+ * @param token The token to use for authentication.
+ */
 export function createApi(token: string) {
     const client = axios.create({
         baseURL: "https://the-one-api.dev/v2",
@@ -117,37 +148,76 @@ export function createApi(token: string) {
     });
 
     return {
+        /**
+         * List of all "The Lord of the Rings" books.
+         */
         books: () => new ApiRequestMany<Book>("book", client),
+        /**
+         * Request one specific book.
+         */
         book: (id: string) => new class BookRequest extends ApiRequestSingle<Book> {
             constructor() { super(`book/${id}`, client) }
 
+            /**
+             * Request all chapters of one specific book.
+             */
             chapters() {
                 return new ApiRequestMany<Chapter>(this.path + "/chapter", client);
             }
         },
 
+        /**
+         * List of all movies, including the "The Lord of the Rings" and the "The Hobbit" trilogies.
+         */
         movies: () => new ApiRequestMany<Movie>("movie", client),
+        /**
+         * Request one specific movie.
+         */
         movie: (id: string) => new class MovieRequest extends ApiRequestSingle<Movie> {
             constructor() { super(`movie/${id}`, client) }
 
+            /**
+             * Request all movie quotes for one specific movie (only working for the LotR trilogy).
+             */
             quotes() {
                 return new ApiRequestMany<Quote>(this.path + "/quote", client);
             }
         },
 
+        /**
+         * List of characters including metadata like name, gender, realm, race and more.
+         */
         characters: () => new ApiRequestMany<Character>("character", client),
+        /**
+         * Request one specific character.
+         */
         character: (id: string) => new class CharacterRequest extends ApiRequestSingle<Character> {
             constructor() { super(`character/${id}`, client) }
 
+            /**
+             * Request all movie quotes of one specific character.
+             */
             quotes() {
                 return new ApiRequestMany<Quote>(this.path + "/quote", client);
             }
         },
 
+        /**
+         * List of all movie quotes.
+         */
         quotes: () => new ApiRequestMany<Quote>("quote", client),
+        /**
+         * Request one specific movie quote.
+         */
         quote: (id: string) => new ApiRequestSingle<Quote>(`quote/${id}`, client),
 
+        /**
+         * List of all book chapters.
+         */
         chapters: () => new ApiRequestMany<Chapter>("chapter", client),
+        /**
+         * Request one specific book chapter.
+         */
         chapter: (id: string) => new ApiRequestSingle<Chapter>(`chapter/${id}`, client),
     }
 }
