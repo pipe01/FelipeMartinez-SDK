eb2250e50bc850d4ddf0ff98dd598f05871697d8
Felipe Martinez <pipernene@gmail.com>
2022-09-10 13:56:29 +0200

    Add remaining models

--- src/models.ts
+++ src/models.ts
@@ -1,17 +1,44 @@
 export interface Model {
-    _id: String;
+    _id: string;
 }
 
 export interface Book extends Model {
-    name: String;
+    name: string;
 }
 
 export interface Movie extends Model {
-    name: String;
+    name: string;
     runtimeInMinutes: number;
     budgetInMillions: number;
     boxOfficeRevenueInMillions: number;
     academyAwardNominations: number;
     academyAwardWins: number;
     rottenTomatoesScore: number;
+}
+
+export interface Character extends Model {
+    height: string;
+    race: string;
+    gender: string;
+    birth: string;
+    spouse: string;
+    death: string;
+    realm: string;
+    hair: string;
+    name: string;
+    wikiUrl: string;
+}
+
+export interface Quote extends Model {
+    dialog: string;
+    movie: string;
+    character: string;
+}
+
+export interface BookChapter extends Model {
+    chapterName: string;
+}
+
+export interface Chapter extends BookChapter {
+    book: string;
 }
